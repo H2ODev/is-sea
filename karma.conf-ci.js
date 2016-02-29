c2bbc7a826e2dc84f8a527028fdbfbd2a037028b
Marvin Oßwald <mo@h2om.de>
2016-02-29 14:51:10 +0100

    adding travis

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -14,21 +14,31 @@ module.exports = function(config) {
   }
 
   // Browsers to run on Sauce Labs
-  var customLaunchers = {
-    'SL_Chrome': {
+  var customLaunchers = {                                 // 1
+    sl_chrome: {
       base: 'SauceLabs',
-      browserName: 'chrome'
+      browserName: 'chrome',
+      platform: 'Windows 7',
+      version: '35'
     },
-    'SL_InternetExplorer': {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      version: '10'
-    },
-    'SL_FireFox': {
+    sl_firefox: {
       base: 'SauceLabs',
       browserName: 'firefox',
+      version: '30'
+    },
+    sl_ios_safari: {
+      base: 'SauceLabs',
+      browserName: 'iphone',
+      platform: 'OS X 10.9',
+      version: '7.1'
+    },
+    sl_ie_11: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 8.1',
+      version: '11'
     }
-  };
+};
 
   config.set({
 
